4cee15da49b74c1bf3aa732154003b447851a159
Estebanm1812 <EstebanMendoza02@hotmail.com>
2022-10-08 18:25:07 -0500

    profile activity creation started

---
+++ app/src/main/java/tech/devpachon/cookbook/profileActivity.kt
@@ -0,0 +1,11 @@
+package tech.devpachon.cookbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
