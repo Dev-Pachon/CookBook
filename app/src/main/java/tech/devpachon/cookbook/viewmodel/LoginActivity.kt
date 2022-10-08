7519d4d8845c7d5551ea32a93a7e9c4cbab19476
Estebanm1812 <EstebanMendoza02@hotmail.com>
2022-10-08 12:17:53 -0500

    welcome screen added

---
+++ app/src/main/java/tech/devpachon/cookbook/viewmodel/LoginActivity.kt
@@ -0,0 +1,12 @@
+package tech.devpachon.cookbook.viewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tech.devpachon.cookbook.R
+
+class loginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
